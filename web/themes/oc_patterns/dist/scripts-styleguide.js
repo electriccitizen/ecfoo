9e54e28a035d246bd777fe0759bd4df230409a47
Tim Broeker <tim@electriccitizen.com>
2017-09-12 15:29:22 -0500

    add styleintrc

--- web/themes/oc_patterns/dist/scripts-styleguide.js
+++ web/themes/oc_patterns/dist/scripts-styleguide.js
@@ -8,19 +8,6 @@
 // };
 
 
-(function ($) {
-//header-background
-Drupal.behaviors.bannerBackground = {
-  attach: function (context, settings) {
-    $(".field-use-as-background", context).once('bannerBackground').each(function(){  
-      if($(this).text() == 'Background'){
-        $( '.block-region-top' ).addClass('background');
-      }
-      $(this).remove();
-    });
-  }
-};
-})(jQuery);
 (function($, Drupal) {
 
 /* RESPONSIVE TABLES WITH STACKTABLE
@@ -165,6 +152,127 @@ Drupal.behaviors.sticky_footer = {
   }
 }; /////end fixed footer //////
 
+})(jQuery, Drupal);
+
+(function ($) {
+//header-background
+Drupal.behaviors.bannerBackground = {
+  attach: function (context, settings) {
+    $(".field-use-as-background", context).once('bannerBackground').each(function(){  
+      if($(this).text() == 'Background'){
+        $( '.block-region-top' ).addClass('background');
+      }
+      $(this).remove();
+    });
+  }
+};
+})(jQuery);
+(function($, Drupal) {
+//search toggle
+Drupal.behaviors.searchToggle = {
+	attach: function (context, settings) {
+	 	$(".block-search-form-block").once('tSearch').each(function(){  
+	 	$('.t-search', this).click(function(){
+        	$(this).toggleClass('searching');
+	 			$('#search-block-form').toggleClass('slideDown');
+	 		});	
+		});
+	}
+}
+})(jQuery, Drupal);
+
+(function($, Drupal) {
+Drupal.behaviors.pageMenu = {
+	attach: function (context, settings) {
+	 	$(".block--system-menu.page-menu", context).once('page-menu').each(function(){  
+	 		//need doc ready because active-class script fires after theme scripts
+      $(document).ready(function(){
+        $('.block--system-menu.page-menu > ul li').each(function(){
+          
+          //find active links and set the active trail
+          $('.is-active', this).parentsUntil('.page-menu').addClass('active-trail expanded');
+
+          //find nested lists and set their parents and expanders
+          if(($('> ul', this).length) && (!$('.expander:first', this).length) ){
+            $(this).addClass('parent').prepend('<span class="expander" aria-expanded="false"></span>');
+          }
+
+          //find active-trail li and add aria expanded role
+          $('li.active-trail > .expander').attr('aria-expanded', "true");
+        });
+        //change expanded class and aria-roles on expander click
+        $('.expander').click(function(){
+          $(this).closest('li').toggleClass('expanded');
+          if($(this).attr('aria-expanded') == 'false'){
+            $(this).attr('aria-expanded', "true");
+          }else{
+            $(this).attr('aria-expanded', "false");
+          }
+        });
+      });
+
+	 		var menu_page = $('.block--system-menu.page-menu');
+	 		var menu = $('.block--system-menu.page-menu > ul');
+
+	 		//add a select list to the page menu for mobile
+      $(window).on('resize',  _.debounce( pageMenuMobile, 100 )).trigger('resize');
+
+      function pageMenuMobile(){
+          var wwidth = $(window).width();
+          
+          //if we're at tablet or less
+          if ((wwidth < 760) || ($('.node:not(.pl-spacer) .kermit').length)){
+
+            //insert a select list if it doesn't already exist
+            if(!$('.block--system-menu.page-menu select').length){
+              $('<select />').appendTo('.block--system-menu.page-menu');
+
+              //add a default first option to that select list
+              $('<option />', {
+                "value"   : "#",
+                "text"    : "Go to Another Page"
+              }).prependTo('.block--system-menu.page-menu select');
+
+
+              // Populate the rest of the dropdown with menu items
+              $('.block--system-menu.page-menu a').each(function() {
+                var el = $(this);
+                $('<option />', {
+                  "value"   : el.attr("href"),
+                  "text"    : el.text()
+                }).appendTo('.block--system-menu.page-menu select');
+              });
+
+              //run select2 on page menu
+              if(!$('.block--system-menu.page-menu .select2-container').length){
+                $('.block--system-menu.page-menu select').select2();
+              }
+
+              //jump to the page when an item is selected
+              $('.block--system-menu.page-menu').find('select').change(function() {
+                window.location = $(this).find('option:selected').val();
+              });
+
+              //detach the page menu wrapper
+              $('.block--system-menu.page-menu > ul').detach();
+
+              
+              
+            }
+
+          }else{
+            //if we're on desktop, remove the select list and reattach the menu block wrapper if its not already there
+            $('.block--system-menu.page-menu:not(.preserve)').find('select').remove();
+            $('.block--system-menu.page-menu:not(.preserve)').find('.select2').remove();
+            if(!$('.block--system-menu.page-menu > ul').length){
+              $('.block--system-menu.page-menu').append(menu);
+            }
+          }
+        }//end pageMenuMobile
+
+ 		});
+	}
+}
 })(jQuery, Drupal);
 
 (function($, Drupal) {
@@ -2831,114 +2939,6 @@ Drupal.behaviors.revealWidget = {
 
 
 })(jQuery, Drupal);
-(function($, Drupal) {
-//search toggle
-Drupal.behaviors.searchToggle = {
-	attach: function (context, settings) {
-	 	$(".block-search-form-block").once('tSearch').each(function(){  
-	 	$('.t-search', this).click(function(){
-        	$(this).toggleClass('searching');
-	 			$('#search-block-form').toggleClass('slideDown');
-	 		});	
-		});
-	}
-}
-})(jQuery, Drupal);
-
-(function($, Drupal) {
-Drupal.behaviors.pageMenu = {
-	attach: function (context, settings) {
-	 	$(".block--system-menu.page-menu", context).once('page-menu').each(function(){  
-	 		//need doc ready because active-class script fires after theme scripts
-      $(document).ready(function(){
-        $('.block--system-menu.page-menu > ul li').each(function(){
-          
-          //find active links and set the active trail
-          $('.is-active', this).parentsUntil('.page-menu').addClass('active-trail expanded');
-
-          //find nested lists and set their parents and expanders
-          if(($('> ul', this).length) && (!$('.expander:first', this).length) ){
-            $(this).addClass('parent').prepend('<span class="expander" aria-expanded="false"></span>');
-          }
-
-          //find active-trail li and add aria expanded role
-          $('li.active-trail > .expander').attr('aria-expanded', "true");
-        });
-        //change expanded class and aria-roles on expander click
-        $('.expander').click(function(){
-          $(this).closest('li').toggleClass('expanded');
-          if($(this).attr('aria-expanded') == 'false'){
-            $(this).attr('aria-expanded', "true");
-          }else{
-            $(this).attr('aria-expanded', "false");
-          }
-        });
-      });
-
-	 		var menu_page = $('.block--system-menu.page-menu');
-	 		var menu = $('.block--system-menu.page-menu > ul');
-
-	 		//add a select list to the page menu for mobile
-      $(window).on('resize',  _.debounce( pageMenuMobile, 100 )).trigger('resize');
-
-      function pageMenuMobile(){
-          var wwidth = $(window).width();
-          
-          //if we're at tablet or less
-          if ((wwidth < 760) || ($('.node:not(.pl-spacer) .kermit').length)){
-
-            //insert a select list if it doesn't already exist
-            if(!$('.block--system-menu.page-menu select').length){
-              $('<select />').appendTo('.block--system-menu.page-menu');
-
-              //add a default first option to that select list
-              $('<option />', {
-                "value"   : "#",
-                "text"    : "Go to Another Page"
-              }).prependTo('.block--system-menu.page-menu select');
-
-
-              // Populate the rest of the dropdown with menu items
-              $('.block--system-menu.page-menu a').each(function() {
-                var el = $(this);
-                $('<option />', {
-                  "value"   : el.attr("href"),
-                  "text"    : el.text()
-                }).appendTo('.block--system-menu.page-menu select');
-              });
-
-              //run select2 on page menu
-              if(!$('.block--system-menu.page-menu .select2-container').length){
-                $('.block--system-menu.page-menu select').select2();
-              }
-
-              //jump to the page when an item is selected
-              $('.block--system-menu.page-menu').find('select').change(function() {
-                window.location = $(this).find('option:selected').val();
-              });
-
-              //detach the page menu wrapper
-              $('.block--system-menu.page-menu > ul').detach();
-
-              
-              
-            }
-
-          }else{
-            //if we're on desktop, remove the select list and reattach the menu block wrapper if its not already there
-            $('.block--system-menu.page-menu:not(.preserve)').find('select').remove();
-            $('.block--system-menu.page-menu:not(.preserve)').find('.select2').remove();
-            if(!$('.block--system-menu.page-menu > ul').length){
-              $('.block--system-menu.page-menu').append(menu);
-            }
-          }
-        }//end pageMenuMobile
-
- 		});
-	}
-}
-})(jQuery, Drupal);
-
 (function($, Drupal) {
 /* distinguish video from other oc media */
 Drupal.behaviors.ocMediaEmbedded = {
